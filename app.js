b42af609eb129abcd4370a638fe4871539b1b747
Omadbek571 <omadbektomsk08@gmail.com>
2024-05-26 22:28:39 +0500

    .

---
+++ app.js
@@ -0,0 +1,318 @@
+// 1-savol
+
+// let n = +prompt("ILTIMOS SON KIRITING");
+// for (let i = 1; i <= n; i++) {
+//     document.write(i);
+//     console.log(i);
+// }
+
+
+// 2-savol
+
+// let n = +prompt("Iltimos son kiriting");
+
+// for (let i = n; i >= 1; i--) {
+//     document.write(i)
+//     console.log(i);
+// }
+
+
+// 3-savol
+
+// let n = +prompt("ILTIMOS SON KIRITING");
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+// }
+
+// console.log(sum);
+// document.write(sum)
+
+
+// 4-savol
+
+// let num = +prompt("Iltimos son kiriting");
+// let sum = 0;
+
+
+// for (let i = 1; i <= num; i++) {
+//     if (i % 2 == 0) {
+//         sum += i
+//         console.log(i);
+//     }
+
+// }
+
+// console.log(sum);
+// document.write("YIGINDISI KIRITGAN SONINGIZDI  "+sum)
+
+
+// 5-savol
+
+
+// let n = 5;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 1) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+// document.write("Yiginisi kiritgan soningizdi  "+sum)
+
+
+// 6-savol
+
+// let num1 = +prompt("Iltimos son kiriting");
+
+// for (let i = 1; i <= num1; i++) {
+//     if (i % 2 === 0) {
+//         document.write(i);
+//         console.log(i);
+
+//     }
+// }
+
+
+// 7-savol
+
+// let num1 = 5;
+// let sum = 0;
+
+// for (let i = 1; i <= num1; i++) {
+//     if (i % 2 === 0) {
+//         sum += i
+//     }
+// }
+// console.log(sum);
+// document.write(sum)
+
+
+// 8-savol
+
+// SHU SAVOLDI YAHSHI TUSHUNA OLMADIM SHUNISIGA YORDAM BERISHTI
+
+// let number = 29;
+// let isPrime = true;
+
+// if (number <= 1) {
+//     isPrime = false;
+// } else if (number <= 3) {
+//     isPrime = true;
+// } else if (number % 2 === 0 || number % 3 === 0) {
+//     isPrime = false;
+// } else {
+//     for (let i = 5; i * i <= number; i += 6) {
+//         if (number % i === 0 || number % (i + 2) === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+// }
+
+// if (isPrime) {
+//     console.log(number + " tub son.");
+// } else {
+//     console.log(number + " tub son emas.");
+// }
+
+
+// SHUNISINI OZIM OYLAGAN HOLDA YOZDIM 
+
+// let num1 = +prompt("Iltimos son kiriting");
+
+// if (num1 > 0) {
+//     console.log("Tub son  " + num1);
+//     document.write("Tub son  " + num1)
+// } else {
+//     console.log("Tub emas son " + num1);
+//     document.write("Tub emas son  " + num1)
+// }
+
+// 9-savol
+
+// let k = +prompt("Iltimos k sonini kiriting");
+// let n = +prompt("Iltimos n sonini kiriting"); 
+
+// for (let i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
+
+// 10-savol
+
+// let a = +prompt("Iltimos a sonini kiriting"); 
+// let b = +prompt("Iltimos b sonini kiriting"); 
+
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//     console.log(i);
+//     count++;
+// }
+
+// console.log("Chiqarilgan sonlar soni: " + count);
+
+
+// 11-savol
+
+// let a = 5; 
+// let b = 10; 
+// let count = 0; 
+
+// for (let i = b - 1; i > a; i--) {
+//     console.log(i);
+//     count++;
+// }
+
+// console.log("Chiqarilgan sonlar soni: " + count);
+
+
+
+// 12-savol
+
+// let kg = 3.5;
+
+// for (let i = 1; i <= 10; i++) {
+//     let price = kg * i;
+//     console.log(i + " kg konfetning narxi: " + price + " so'm");
+// }
+
+
+// 13-savol
+
+// let kg = 3.5; 
+
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//     let price = kg * i;
+//     console.log(i.toFixed(1) + " kg konfetning narxi: " + price.toFixed(2) + " so'm");
+// }
+
+
+// 14-savol
+
+
+// let kg = 3.5; 
+
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//     let price = kg * i;
+//     console.log(i.toFixed(1) + " kg konfetning narxi: " + price.toFixed(2) + " so'm");
+// }
+
+
+// 15-savol
+
+
+// let a = 5; 
+// let b = 10; 
+
+// let sum = 0; 
+
+// for (let i = a; i <= b; i++) {
+//     sum += i;
+// }
+
+// console.log("a dan b gacha bo'lgan sonlar yig'indisi: " + sum);
+
+
+// 16-savol
+
+// let a = 2; 
+// let b = 6; 
+
+// let product = 1; 
+
+// for (let i = a; i <= b; i++) {
+//     product *= i;
+// }
+
+// console.log("a dan b gacha bo'lgan sonlar ko'paytmasi: " + product);
+
+
+// 17-savol
+
+// let a = 3; 
+// let b = 6; 
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//     sum += i * i;
+// }
+
+// console.log("a dan b gacha bo'lgan sonlar kvadratlarining yig'indisi: " + sum);
+
+
+// 18-savol
+
+// let n = 5; 
+// let sum = 0; 
+
+// for (let i = 1; i <= n; i++) {
+//     sum += 1 / i;
+// }
+
+// console.log("Yig'indisi: " + sum);
+
+
+// 19-savol
+
+
+// let n = 5; 
+
+// let sum = 0; 
+// for (let i = n; i <= 2 * n; i++) {
+//     sum += i * i;
+// }
+
+// console.log("Yig'indisi: " + sum);
+
+
+// 20-savol
+
+// let n = 5; 
+
+// let sum = 1; 
+
+// for (let i = 1.1; i <= 1.1 + (n - 1) * 0.1; i += 0.1) {
+//     sum *= i;
+// }
+
+// console.log("Ko'paytma: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
